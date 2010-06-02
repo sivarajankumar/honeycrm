132eb65b702af15d73844c4c23fc41dbaa384578
ingo.jaeckel <ingo.jaeckel@dbaa7660-4561-eb87-3f7d-5d2331463a75>
2010-06-02 09:56:45 +0000

    fixed module name in detail view test case

--- trunk/src/crm/client/test/DetailViewTest.java
+++ trunk/src/crm/client/test/DetailViewTest.java
@@ -1,18 +1,32 @@
 package crm.client.test;
 
 import com.google.gwt.junit.client.GWTTestCase;
+import com.google.gwt.user.client.Timer;
 
 import crm.client.dto.DtoContact;
 import crm.client.view.DetailView;
 
 public class DetailViewTest extends GWTTestCase {
 	public void testA() {
-		DetailView d = new DetailView(DtoContact.class);
+		final DetailView d = new DetailView(DtoContact.class);
+		d.refresh(1);
 		assertNotNull(d);
+		System.out.println("is not null");
+
+		new Timer() {
+			@Override
+			public void run() {
+				assertTrue(d.isVisible());
+				System.out.println("is visible");
+				finishTest();
+			}
+		}.schedule(200);
+
+		delayTestFinish(1000);
 	}
 
 	@Override
 	public String getModuleName() {
-		return "crm.client.Gae";
+		return "crm.Gae";
 	}
 }
