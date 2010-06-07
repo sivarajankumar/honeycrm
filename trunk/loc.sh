f66836e297623ceb09f147f34ae30ebb70422627
ingo.jaeckel <ingo.jaeckel@dbaa7660-4561-eb87-3f7d-5d2331463a75>
2010-06-07 13:51:01 +0000

    little layout issues fixed in custom.css file. fixed web.xml file after changing crm -> honeycrm. added loc for core system

--- trunk/loc.sh
+++ trunk/loc.sh
@@ -2,6 +2,9 @@
 echo "overall lines of code:"
 find src/crm/ -name "*.java" | xargs wc -l | sort -n
 echo ""
+echo "core system (everything except dtos, domain classes, tests):"
+find src/ -name "*.java" | grep -v "client/dto" | grep -v "server/domain" | grep -v "client/test" | grep -v "server/test" | xargs wc -l | sort -n
+echo ""
 echo "client test code only:"
 find src/crm/client/test -name "*.java" | xargs wc -l | tail -n 1 
 echo ""
