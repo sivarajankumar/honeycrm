e02d49c4a42e6c32f21b4f739421f41a3a0f24af
ingo.jaeckel <ingo.jaeckel@dbaa7660-4561-eb87-3f7d-5d2331463a75>
2010-07-05 07:55:03 +0000

    added test case for setting relatesto annotation

---
+++ src/honeycrm/server/test/RelatesToTest.java
@@ -0,0 +1,44 @@
+package honeycrm.server.test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import honeycrm.server.CachingReflectionHelper;
+import honeycrm.server.ReflectionHelper;
+import honeycrm.server.domain.AbstractEntity;
+import honeycrm.server.domain.decoration.RelatesTo;
+import junit.framework.TestCase;
+
+public class RelatesToTest extends TestCase {
+	public void testRelatesToIsSet() {
+		try {
+			boolean failed = false;
+
+			final ReflectionHelper reflectionHelper = new CachingReflectionHelper();
+
+			for (final Class<? extends AbstractEntity> clazz : ReflectionHelper.getClasses("honeycrm.server.domain")) {
+				if (Modifier.isAbstract(clazz.getModifiers())) {
+					continue;
+				}
+
+				for (final Field field : reflectionHelper.getAllFields(clazz)) {
+					final boolean isLong = Long.class.equals(field.getType()) || long.class.equals(field.getType());
+					final boolean hasCorrectName = field.getName().toLowerCase().contains("id");
+					final boolean isAnnotated = field.isAnnotationPresent(RelatesTo.class);
+
+					if (isLong && hasCorrectName && !isAnnotated) {
+						System.err.println(clazz.getSimpleName() + "." + field.getName() + " looks like a foreign key field with no " + RelatesTo.class.getSimpleName() + " annotation set.");
+						failed = true;
+					}
+				}
+			}
+
+			if (failed) {
+				fail();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+}
