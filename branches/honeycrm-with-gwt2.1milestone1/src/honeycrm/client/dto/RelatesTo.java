8e0f433bfc1a3ca3e162e994b93f141334630a07
ingo.jaeckel <ingo.jaeckel@dbaa7660-4561-eb87-3f7d-5d2331463a75>
2010-07-23 16:36:30 +0000

    removed older honeycrm gwt 2.1 m1 branch

--- branches/honeycrm-with-gwt2.1milestone1/src/honeycrm/client/dto/RelatesTo.java
+++
@@ -1,9 +0,0 @@
-package honeycrm.client.dto;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface RelatesTo {
-	Class<? extends AbstractDto> value();
-}
