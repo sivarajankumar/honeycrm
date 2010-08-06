4c5f8e2ed22bd814bfc196bbd2f82b5a87a913b3
ingo.jaeckel <ingo.jaeckel@dbaa7660-4561-eb87-3f7d-5d2331463a75>
2010-08-06 09:46:59 +0000

    readded modified DtoCopyMachinePerformanceTest class

---
+++ branches/honeycrm_r134_gwt2.1m2/src/honeycrm/server/test/DtoCopyMachinePerformanceTest.java
@@ -0,0 +1,37 @@
+package honeycrm.server.test;
+
+import honeycrm.client.dto.Dto;
+import honeycrm.server.DemoDataProvider;
+import honeycrm.server.domain.AbstractEntity;
+import honeycrm.server.transfer.DtoCopyMachine;
+
+public class DtoCopyMachinePerformanceTest {
+	private static final int ENTITY_COUNT = 800000;
+
+	public static void main(String[] args) {
+		final DtoCopyMachine copy = new DtoCopyMachine();
+		System.out.print("Get demo accounts..");
+		final AbstractEntity[] accounts = getDemoAccounts();
+		final Dto[] dtos = new Dto[ENTITY_COUNT];
+
+		final long before = System.currentTimeMillis();
+		System.out.print("Done.\nCopying..");
+		for (int i = 0; i < ENTITY_COUNT; i++) {
+			dtos[i] = copy.copy(accounts[i]);
+		}
+		final long diff = System.currentTimeMillis() - before;
+
+		final double perEntity = (double) diff / (double) ENTITY_COUNT;
+		final String timePerEntity = String.format("%.3f", 100.0 * perEntity);
+		System.out.println("Done.\nDuration: " + diff + " ms, " + timePerEntity + " ms for 100 entities");
+	}
+
+	private static AbstractEntity[] getDemoAccounts() {
+		final AbstractEntity[] accounts = new AbstractEntity[ENTITY_COUNT];
+		for (int i = 0; i < ENTITY_COUNT; i++) {
+			accounts[i] = DemoDataProvider.account();
+		}
+		return accounts;
+	}
+
+}
