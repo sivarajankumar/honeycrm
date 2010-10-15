b8128d0f2f6bbc2c49ac867c8e565e8cea590de6
ingo.jaeckel <ingo.jaeckel@dbaa7660-4561-eb87-3f7d-5d2331463a75>
2010-10-15 11:56:28 +0000

    do onModuleLoad for plugins asynchronous -> fetch plugin code when neccessary added documentation about when gwt-exporter is really needed

--- branches/PlatformTest/src/platform/client/PluginRegistry.java
+++ branches/PlatformTest/src/platform/client/PluginRegistry.java
@@ -9,7 +9,16 @@ public class PluginRegistry implements Exportable {
 	private static final HashSet<AbstractPlugin> plugins = new HashSet<AbstractPlugin>();
 
 	@Export
+	//
 	// Only (!) export this method.
+	//
+	// This only has to be exported if the plugins are compiled separately
+	// If they are compiled as part of the platform we can use usual JSNI without the GWT-Exporter.
+	// The AbstractPlugin.loadNewPluginObject code then would have to be changed to the syntax described on
+	// http://code.google.com/intl/de-DE/webtoolkit/doc/latest/DevGuideCodingBasicsJSNI.html#methods-fields
+	//
+	// @<package>.<class>::<staticMethod> ...
+	//
 	public static void registerPluginObject(final AbstractPlugin plugin) {
 		plugins.add(plugin);
 	}
