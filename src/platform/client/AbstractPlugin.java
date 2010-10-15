60b2de19ffd99e31c72bb563de5e5c82a5eac4d5
ingo.jaeckel <ingo.jaeckel@dbaa7660-4561-eb87-3f7d-5d2331463a75>
2010-10-15 11:56:28 +0000

    do onModuleLoad for plugins asynchronous -> fetch plugin code when neccessary added documentation about when gwt-exporter is really needed

--- src/platform/client/AbstractPlugin.java
+++ src/platform/client/AbstractPlugin.java
@@ -1,23 +1,37 @@
 package platform.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Widget;
 
 abstract public class AbstractPlugin implements EntryPoint {
 	private native void loadNewPluginObject(final AbstractPlugin plugin) /*-{
 		$wnd.platform.PluginRegistry.registerPluginObject(plugin);
 	}-*/;
-	
+
 	@Override
 	public void onModuleLoad() {
 		final AbstractPlugin p = this;
-		new Timer() {
+
+		GWT.runAsync(new RunAsyncCallback() {
 			@Override
-			public void run() {
-				loadNewPluginObject(p);
+			public void onSuccess() {
+				new Timer() {
+					@Override
+					public void run() {
+						loadNewPluginObject(p);
+					}
+				}.schedule(2 * 1000);
 			}
-		}.schedule(2 * 1000);
+
+			@Override
+			public void onFailure(Throwable reason) {
+				Window.alert("Could not load code " + this.getClass().toString());
+			}
+		});
 	}
 
 	final public String getName() {
