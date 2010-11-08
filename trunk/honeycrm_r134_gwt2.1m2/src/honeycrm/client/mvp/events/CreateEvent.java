24b9615abf6d43c4fd01c09aa24de564512df2a4
ingo.jaeckel <ingo.jaeckel@dbaa7660-4561-eb87-3f7d-5d2331463a75>
2010-11-08 21:24:16 +0000

    allow storing a map of prefilled fields in the create event

--- trunk/honeycrm_r134_gwt2.1m2/src/honeycrm/client/mvp/events/CreateEvent.java
+++ trunk/honeycrm_r134_gwt2.1m2/src/honeycrm/client/mvp/events/CreateEvent.java
@@ -1,13 +1,22 @@
 package honeycrm.client.mvp.events;
 
+import java.util.HashMap;
+
 import com.google.gwt.event.shared.GwtEvent;
 
 public class CreateEvent extends GwtEvent<CreateEventHandler> {
 	public static Type<CreateEventHandler> TYPE = new Type<CreateEventHandler>();
 	private final String module;
+	private final HashMap<String, Object> prefilledFields;
 
 	public CreateEvent(String module) {
 		this.module = module;
+		this.prefilledFields = null;
+	}
+	
+	public CreateEvent(String module, final HashMap<String, Object> prefilledFields) {
+		this.module = module;
+		this.prefilledFields = prefilledFields;
 	}
 
 	@Override
@@ -22,5 +31,9 @@ public class CreateEvent extends GwtEvent<CreateEventHandler> {
 	
 	public String getModule() {
 		return module;
+	}
+	
+	public HashMap<String, Object> getPrefilledFields() {
+		return prefilledFields;
 	}
 }
