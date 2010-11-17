321afefdfd9ba674c2ade75dbbedee61bf7e0ce6
ingo.jaeckel <ingo.jaeckel@dbaa7660-4561-eb87-3f7d-5d2331463a75>
2010-11-17 16:09:55 +0000

    generalized pdf generation for all modules  -  still very basic but it works like charm

--- honeycrm_r134_gwt2.1m2/src/honeycrm/server/services/Pdf.java
+++ honeycrm_r134_gwt2.1m2/src/honeycrm/server/services/Pdf.java
@@ -1,7 +1,10 @@
 package honeycrm.server.services;
 
-import java.io.FileOutputStream;
+import honeycrm.client.dto.Dto;
+
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -9,45 +12,90 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Font.FontFamily;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class Pdf extends HttpServlet {
 	private static final long serialVersionUID = -5755890768164436414L;
+	private static final ReadServiceImpl readService = new ReadServiceImpl();
 
+	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 	}
 
+	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
+	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("application/pdf"); // Code 1
-		Document document = new Document();
 		try {
-			PdfWriter.getInstance(document, response.getOutputStream()); // Code 2
+			Document document = new Document();
+			PdfWriter.getInstance(document, response.getOutputStream());
+
+			document.addCreationDate();
 			document.open();
 
-			// Code 3
-			PdfPTable table = new PdfPTable(2);
-			table.addCell("1");
-			table.addCell("2");
-			table.addCell("3");
-			table.addCell("4");
-			table.addCell("5");
-			table.addCell("6");
+			final String module;
+			final long id;
 
-			// Code 4
+			String values[] = request.getParameterValues("m");
+			if (null != values && 1 == values.length) {
+				module = values[0];
+			} else {
+				module = null;
+			}
+			values = request.getParameterValues("id");
+			if (null != values && 1 == values.length) {
+				id = Long.parseLong(values[0]);
+			} else {
+				id = -1;
+			}
+			response.setContentType("application/pdf");
+			response.setHeader("Content-Disposition", "inline; filename=" + module + ".pdf"); // TODO add more information about the entity into the filename.
+
+			final Dto dto = readService.get(module, id);
+
+			for (final Entry<String, Serializable> entry : dto.getAllData().entrySet()) {
+				final String value = String.valueOf((entry.getKey().endsWith("_resolved")) ? ((Dto) entry.getValue()).get("name") : entry.getValue());
+				document.add(getAttributeParagraph(entry.getKey(), value));
+			}
+
+			PdfPTable table = new PdfPTable(2);
+			table.setHeaderRows(1);
+			table.addCell("Foo");
+			table.addCell("Bar");
+			for (int i = 0; i < 100; i++) {
+				table.addCell(String.valueOf(i));
+				table.addCell(String.valueOf(i + 1));
+			}
+			document.add(new Paragraph("Offering", new Font(FontFamily.HELVETICA, 20, Font.BOLD)));
+
+			document.add(getAttributeParagraph("Description", "The description"));
+			document.add(getAttributeParagraph("Assigned To", "James"));
+
+			document.add(new Chunk(Chunk.NEWLINE));
 			document.add(table);
 			document.close();
-		} catch (DocumentException e) {
-			e.printStackTrace();
+		} catch (DocumentException ex) {
+			ex.printStackTrace();
 		}
+	}
+
+	protected Element getAttributeParagraph(final String key, final String value) {
+		final Paragraph p = new Paragraph();
+		p.add(new Chunk(key + ": ", new Font(FontFamily.HELVETICA, 12, Font.BOLD)));
+		p.add(new Chunk(value));
+		return p;
 	}
 
 }
