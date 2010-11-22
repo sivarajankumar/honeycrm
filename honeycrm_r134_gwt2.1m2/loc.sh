bf17ccb9706d4e50d9e2d4b035b5b3c867c4d00d
ingo.jaeckel <ingo.jaeckel@dbaa7660-4561-eb87-3f7d-5d2331463a75>
2010-11-22 14:52:29 +0000

    only display overall LOC in loc.sh output

--- honeycrm_r134_gwt2.1m2/loc.sh
+++ honeycrm_r134_gwt2.1m2/loc.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-echo "overall lines of code:"
-find src/honeycrm/ -name "*.java" | xargs wc -l | sort -n
-echo ""
-echo "core system (everything except dtos, domain classes, tests):"
-find src/ -name "*.java" | grep -v "client/dto" | grep -v "server/domain" | grep -v "server/test" | xargs wc -l | sort -n
-echo ""
-echo "server test code only:"
+echo
+echo "test code only:"
 find src/honeycrm/server/test -name "*.java" | xargs wc -l | tail -n 1
+echo 
+echo "core system (everything except dtos, domain classes, tests):"
+find src/ -name "*.java" | grep -v "client/dto" | grep -v "server/domain" | grep -v "server/test" | xargs wc -l | sort -n | tail -n 1
+echo
+echo "overall lines of code:"
+find src/honeycrm/ -name "*.java" | xargs wc -l | sort -n | tail -n 1 
+echo
